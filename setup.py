8c56d6698e733aba489f158b5de8a06015d81f6b
Aaron Danielson <aaronjdanielson@ucla.edu>
2023-04-18 09:21:18 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from histoColor import __version__
+
+setup(
+    name='histoColor',
+    version=__version__,
+
+    url='https://github.com/micky-lab/histoColor',
+    author='Aaron Danielson',
+    author_email='aaronjdanielson@protonmail.com',
+
+    py_modules=['histoColor'],
+)
